9e9154a3ad6e1c5afe1db081bddd1e7952771347
丁志超 <948351520@qq.com>
2021-06-10 14:39:11 +0800

    no message

---
+++ tool.demo/src/main/zk/com/zk/curator/ZkCuratorLock.java
@@ -0,0 +1,51 @@
+package com.zk.curator;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessLock;
+import org.apache.curator.framework.recipes.locks.InterProcessSemaphoreMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.KeeperException;
+  
+  
+/**
+ * 功能：zk - zk Curator客户端 - 实现分布式锁测试
+ * 作者：丁志超
+ */  
+public class ZkCuratorLock{  
+	
+	//实例化客户端
+	private static RetryPolicy retryPolicy  = new ExponentialBackoffRetry(1000,3);
+    private static CuratorFramework client = CuratorFrameworkFactory.builder()
+            .connectString("zookeeper-dev.idc.yst.com.cn:2181")
+            .sessionTimeoutMs(3000)
+            .connectionTimeoutMs(5000)
+            .retryPolicy(retryPolicy)
+            .build();
+    
+    //zk分布式锁创建节点在零时目录zklock下创建
+    static String lockPath = "/zklock";
+    //实例化分布式锁
+    final static InterProcessLock lock = new InterProcessSemaphoreMutex(client, lockPath);
+	
+	
+	public static void main(String[] args) throws NoSuchAlgorithmException, IOException, KeeperException, InterruptedException {
+		//获取锁
+		try {
+			lock.acquire();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			//释放锁
+			try {
+				lock.release();
+			} catch (Exception e) {
+			}
+		}
+	}
+	
+}  
