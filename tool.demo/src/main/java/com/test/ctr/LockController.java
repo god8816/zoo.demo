94009d76a3bd759d62d0172964ffee312e555e51
丁志超 <948351520@qq.com>
2021-06-14 01:48:48 +0800

    no message

--- tool.demo/src/main/java/com/test/ctr/LockController.java
+++ tool.demo/src/main/java/com/test/ctr/LockController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import com.redis.lock.LuaLock;
 import com.redis.lock.RedissonLock;
 import com.redis.lock.SetNxLock;
+import com.zk.lock.ZkCuratorLock;
 
 /**
  * @描述 分布式锁实现性能测试
@@ -22,7 +23,7 @@ public class LockController {
 
 
     /**
-     * setNx 性能测试
+     * redis Lock 性能测试
      * @return
      */
 	@GetMapping("/redislock")
@@ -38,7 +39,19 @@ public class LockController {
 		
 		//LuaLock 测试 1206TPS
 		LuaLock.lualock(key, key, timeOut);
+    }
+	
+	
+    /**
+     * zk Lock 性能测试
+     * @return
+     */
+	@GetMapping("/zklock")
+    public void zklockTest(){
 		
+		String key = UUID.randomUUID().toString();
+		Integer timeOut = 10;
+		ZkCuratorLock.zkCuratorLock();
     }
 
   
--- tool.demo/src/main/zk/com/zk/lock/ZkCuratorLock.java
+++ tool.demo/src/main/zk/com/zk/lock/ZkCuratorLock.java
@@ -1,16 +1,12 @@
-package com.zk.curator;
+package com.zk.lock;
 
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.locks.InterProcessLock;
 import org.apache.curator.framework.recipes.locks.InterProcessSemaphoreMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
-import org.apache.zookeeper.KeeperException;
-  
-  
+
 /**
  * 功能：zk - zk Curator客户端 - 实现分布式锁测试
  * 作者：丁志超
@@ -28,15 +24,20 @@ public class ZkCuratorLock{
     
     //zk分布式锁创建节点在零时目录zklock下创建
     static String lockPath = "/zklock";
-
-	
-	public static void main(String[] args) throws NoSuchAlgorithmException, IOException, KeeperException, InterruptedException {
-	    //实例化分布式锁
+    
+ 
+    
+    
+	/**
+	 * ZK Curator Lock - 单机版本
+	 * */
+	public static void zkCuratorLock() {
+		//实例化分布式锁
 	    InterProcessLock lock = new InterProcessSemaphoreMutex(client, lockPath);
-		
 		//获取锁
 		try {
 			lock.acquire();
+			System.out.println("我获取了锁，该我干活了。");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -49,4 +50,9 @@ public class ZkCuratorLock{
 		}
 	}
 	
+
+	
+	public static void main(String[] args){
+		zkCuratorLock();
+	}
 }  
